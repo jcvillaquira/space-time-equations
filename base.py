d5a7c8efc339e79fbf849ab7a4cb0c7afaf23e92
jcvillaquira <julian.villaquira@quantil.com.co>
2022-05-26 22:39:17 -0500

    Added stars in bg

--- base.py
+++ base.py
@@ -19,7 +19,12 @@ class Test(Scene):
         eqs_in = [FadeIn(eq) for eq in eqs_tex]
         eqs_scale = [eq.animate.scale(scale_size()) for eq in eqs_tex]
 
-        self.play(*eqs_in, run_time = 0.5)
+        coords_star = [coord_star() for n in range(250)]
+        stars = [Dot(radius=radius_star(), color=color_star(), fill_opacity=opacity_star()) for n in range(1, 250)]
+        stars = [star.shift(c[0]*RIGHT+c[1]*UP) for c, star in zip(coords_star, stars)]
+        stars = [FadeIn(star) for star in stars]
+
+        self.play(*stars, *eqs_in, run_time = 0.5)
         self.wait(0.5)
         self.play(*eqs_scale, run_time = 4.5)
         self.wait(0.5)
@@ -30,8 +35,25 @@ def font_size():
 def scale_size():
     return np.random.uniform(1.0, 1.5)**2
 
-def get_coordinate():
-    return np.random.uniform(-3, 3)
+def get_coordinate(max=3):
+    return np.random.uniform(-max, max)
 
 def opacity():
     return np.random.uniform(0.5, 1.0)**2
+
+def coord_star():
+    x = get_coordinate(8)
+    y = get_coordinate(6)
+    return (x, y)
+
+def radius_star():
+    return np.sqrt(np.random.uniform(0.001, 0.0025))
+
+def color_star():
+    r = np.random.uniform(0.65, 1)
+    g = np.random.uniform(0.65, 1)
+    b = np.random.uniform(0.8, 1)
+    return rgb_to_color([r, g, b])
+
+def opacity_star():
+    return np.random.uniform(0.05, 0.35)
